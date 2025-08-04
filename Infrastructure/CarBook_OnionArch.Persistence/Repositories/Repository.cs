0da42d0a6cf5c4f4b9e61fc80bc8421afe3088f2
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-08-04 11:45:15 +0300

    Repository operations created

---
+++ Infrastructure/CarBook_OnionArch.Persistence/Repositories/Repository.cs
@@ -0,0 +1,41 @@
+﻿using CarBook_OnionArch.Application.Interfaces;
+using CarBook_OnionArch.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarBook_OnionArch.Persistence.Repositories
+{
+    public class Repository<T>(CarBookContext context) 
+        : IRepository<T> where T : class
+    {
+        public async Task CreateAsync(T entity)
+        {
+            context.Set<T>().Add(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await context.Set<T>().FindAsync(id)
+                ?? throw new Exception("Girilen id ile kayıt bulunamadı");
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            var record = await context.Set<T>().FindAsync(id)
+               ?? throw new Exception("Girilen id ile kayıt bulunamadı");
+            context.Set<T>().Remove(record);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            context.Set<T>().Update(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
