4442ee8f260cd3970bb82cf83168a3aca70aabe0
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-08-05 18:46:15 +0300

    UI and Admin themes included

---
+++ Frontends/CarBook_OnionArch.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarBook_OnionArch.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
