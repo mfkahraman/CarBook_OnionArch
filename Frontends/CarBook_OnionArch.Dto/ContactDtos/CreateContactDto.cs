96deaac378162e86597d4335ac9ee268c13dfd75
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-08-06 19:15:45 +0300

    Implemented Send Contact message Feature to UI Contact page

---
+++ Frontends/CarBook_OnionArch.Dto/ContactDtos/CreateContactDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBook_OnionArch.Dto.ContactDtos
+{
+    public class CreateContactDto
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Subject { get; set; }
+        public required string Message { get; set; }
+        public DateTime SendDate { get; set; } = DateTime.Now;
+    }
+}
--- Frontends/CarBook_OnionArch.WebUI/Controllers/ContactController.cs
+++ Frontends/CarBook_OnionArch.WebUI/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook_OnionArch.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
 
 namespace CarBook_OnionArch.WebUI.Controllers
 {
@@ -10,10 +13,18 @@ namespace CarBook_OnionArch.WebUI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(CreateContactDto dto)
         {
             var client = httpClientFactory.CreateClient();
-            var jsonData
+            var jsonData = JsonSerializer.Serialize(dto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7020/api/Contacts/create", content);
+            if(!responseMessage.IsSuccessStatusCode)
+            {
+                return Content("Error occurred while sending the message.");
+            }
+            
+            return RedirectToAction("Index", "Home");
         }
     }
 }
