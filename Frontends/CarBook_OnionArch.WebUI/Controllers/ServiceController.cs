f88a0c5c6bdde89d03852357a925ff7d99a83fd9
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-08-06 16:10:05 +0300

    Implemented dynamic data fetching for Services from the database

---
+++ Frontends/CarBook_OnionArch.WebUI/Controllers/ServiceController.cs
@@ -0,0 +1,22 @@
+﻿using CarBook_OnionArch.Dto.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace CarBook_OnionArch.WebUI.Controllers
+{
+    public class ServiceController(IHttpClientFactory httpClient) : Controller
+    {
+        public IActionResult Index()
+        {
+            var client = httpClient.CreateClient("ServicesClient");
+            var response = client.GetAsync("https://localhost:7020/api/Services");
+            if (!response.Result.IsSuccessStatusCode)
+            {
+                return Content("Error fetching services");
+            }
+            var jsonData = response.Result.Content.ReadAsStringAsync().Result;
+            var values = JsonSerializer.Deserialize<List<ResultServiceDto>>(jsonData);
+            return View(values);
+        }
+    }
+}
