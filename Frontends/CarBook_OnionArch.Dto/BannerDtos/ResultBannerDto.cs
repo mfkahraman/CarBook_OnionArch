edf808858dadaa0d107452f24e288d408ad99a05
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-08-07 21:19:31 +0300

    Implemented Banner section fetching dynamically from db to Homepage

---
+++ Frontends/CarBook_OnionArch.Dto/BannerDtos/ResultBannerDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CarBook_OnionArch.Dto.BannerDtos
+{
+    public class ResultBannerDto
+    {
+            public int bannerId { get; set; }
+            public string? title { get; set; }
+            public string? description { get; set; }
+            public string? videoDescription { get; set; }
+            public string? videoUrl { get; set; }
+
+    }
+}
---
+++ Frontends/CarBook_OnionArch.WebUI/ViewComponents/DefaultViewComponents/DefaultCoverUILayoutComponentPartial.cs
@@ -0,0 +1,24 @@
+﻿using CarBook_OnionArch.Dto.BannerDtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace CarBook_OnionArch.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class DefaultCoverUILayoutComponentPartial(IHttpClientFactory httpClientFactory) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7020/api/Banners/get-all");
+            if(!response.IsSuccessStatusCode)
+            {
+                return Content("Error fetching banners");
+            }
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var values = JsonSerializer.Deserialize<List<ResultBannerDto>>(jsonData)
+                ?? throw new Exception("Banner UI katmanına getirilemedi");
+            var value = values.FirstOrDefault();
+            return View(value);
+        }
+    }
+}
