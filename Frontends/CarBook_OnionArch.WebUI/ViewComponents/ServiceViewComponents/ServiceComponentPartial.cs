1985b3a51af456e9c9a98181c114a48c4fd19b05
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-08-11 13:34:09 +0300

    Integrate new components and localize text
    Updated `Index.cshtml` to replace static content with dynamic components for "About Us," "Services," "Become a Driver," and "Testimonials." Translated text in `Default.cshtml` to Turkish. Introduced `ServiceComponentPartial.cs` for fetching and rendering service data from an external API.

---
+++ Frontends/CarBook_OnionArch.WebUI/ViewComponents/ServiceViewComponents/ServiceComponentPartial.cs
@@ -0,0 +1,24 @@
+﻿using CarBook_OnionArch.Dto.FooterAddressDtos;
+using CarBook_OnionArch.Dto.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace CarBook_OnionArch.WebUI.ViewComponents.ServiceViewComponents
+{
+    public class ServiceComponentPartial(IHttpClientFactory httpClient) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = httpClient.CreateClient("ServicesClient");
+            var response = await client.GetAsync("https://localhost:7020/api/Services");
+            if (!response.IsSuccessStatusCode)
+            {
+                return Content("Error fetching services");
+            }
+            var jsonData = response.Content.ReadAsStringAsync().Result;
+            var values = JsonSerializer.Deserialize<List<ResultServiceDto>>(jsonData);
+            return View(values);
+
+        }
+    }
+}
