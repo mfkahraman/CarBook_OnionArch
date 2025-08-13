6cea5d9392b4f29eb7dcf6bebf967927a6caa26c
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-08-13 11:41:33 +0300

    Add Admin area structure and layout components
    - Updated `CarBook_OnionArch.WebUI.csproj` to include new folders for Admin Data and Models.
    - Added area-based routing in `Program.cs` for better organization.
    - Created `AdminCarController` and `AdminLayoutController` for handling Admin views.
    - Updated `Index.cshtml` to use the new Admin layout.
    - Introduced partial views: `AdminHeaderPartial`, `AdminNavbarPartial`, and `AdminSideBarPartial` for the Admin panel.
    - Created `AdmLayout.cshtml` as the main layout for the Admin area.

---
+++ Frontends/CarBook_OnionArch.WebUI/Areas/Admin/Controllers/AdminLayoutController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook_OnionArch.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult AdmLayout()
+        {
+            return View();
+        }
+
+        public PartialViewResult AdminHeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminNavbarPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminSidebarPartial()
+        {
+            return PartialView();
+        }
+
+
+    }
+}
