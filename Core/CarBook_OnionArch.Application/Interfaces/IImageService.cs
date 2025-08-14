e751305155b60c62e29605fffa24297f0470f1e7
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-08-14 16:57:16 +0300

    Add image service for file handling and update packages
    Updated project files to include new package references for
    `Microsoft.AspNetCore.Http.Features` and
    `Microsoft.Extensions.Hosting.Abstractions`.
    
    Created a new interface `IImageService` for saving and
    deleting image files, and implemented the `ImageService`
    class to provide the defined functionality.

---
+++ Core/CarBook_OnionArch.Application/Interfaces/IImageService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CarBook_OnionArch.Application.Interfaces
+{
+    public interface IImageService
+    {
+        /// <summary>
+        /// Saves an image file from the computer to projects wwwroot/images folder
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns> Returns a strings value for the model's ImageUrl property </returns>
+        Task<string> SaveImageAsync(IFormFile file, string folder);  // returns URL string
+        Task<bool> DeleteImageAsync(string imageUrl);
+    }
+}
---
+++ Infrastructure/CarBook_OnionArch.Persistence/Repositories/ImageService.cs
@@ -0,0 +1,49 @@
+﻿using CarBook_OnionArch.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+namespace CarBook_OnionArch.Persistence.Repositories
+{
+    public class ImageService(IHostEnvironment _env) : IImageService
+    {
+        // Save Image method
+        public async Task<string> SaveImageAsync(IFormFile file, string subFolder)
+        {
+            if (file == null || file.Length == 0)
+                throw new ArgumentException("File cannot be null or empty.", nameof(file));
+
+            var extension = Path.GetExtension(file.FileName);
+            var newFileName = Guid.NewGuid() + extension;
+
+            var absolutePath = Path.Combine(_env.ContentRootPath, "wwwroot", "images", subFolder, newFileName);
+
+            Directory.CreateDirectory(Path.GetDirectoryName(absolutePath)!);
+
+            using (var stream = new FileStream(absolutePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Path.Combine("/images", subFolder, newFileName).Replace("\\", "/");
+        }
+
+        // Delete Image method
+        public async Task<bool> DeleteImageAsync(string imageUrl)
+        {
+            if (string.IsNullOrEmpty(imageUrl))
+                return false;
+
+            // Normalize path
+            var relativePath = imageUrl.TrimStart('/').Replace("/", Path.DirectorySeparatorChar.ToString());
+            var fullPath = Path.Combine(_env.ContentRootPath, "wwwroot", relativePath);
+
+            if (File.Exists(fullPath))
+            {
+                await Task.Run(() => File.Delete(fullPath));
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
