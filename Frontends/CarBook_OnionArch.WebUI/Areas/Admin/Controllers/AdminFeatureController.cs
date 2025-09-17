a021ed4b451d6ea7a2538de520e81e1cecdcd63a
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-09-17 13:27:19 +0300

    Add AdminFeatureController for feature management
    Introduces the `AdminFeatureController` to handle HTTP requests for features in the admin area. Implements the `IndexAsync` method to fetch and display feature data from an API, with error handling for failed requests. Utilizes dependency injection for `IHttpClientFactory`.

---
+++ Frontends/CarBook_OnionArch.WebUI/Areas/Admin/Controllers/AdminFeatureController.cs
@@ -0,0 +1,26 @@
+﻿using CarBook_OnionArch.Dto.FeaturesDtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace CarBook_OnionArch.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminFeatureController(IHttpClientFactory httpClient) : Controller
+    {
+        public async Task<IActionResult> IndexAsync()
+        {
+            var client = httpClient.CreateClient();
+            var response = await client.GetAsync("https://localhost:7020/api/Features");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return View("Error");
+            }
+
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var features = JsonSerializer.Deserialize<List<ResultFeatureDto>>(jsonData);
+
+            return View(features);
+        }
+    }
+}
