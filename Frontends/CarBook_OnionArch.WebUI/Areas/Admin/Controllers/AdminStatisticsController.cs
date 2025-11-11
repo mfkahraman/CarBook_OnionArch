ea45e77b6055898cbfd919fa03e9a352f59ebc85
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-11-11 21:05:02 +0300

    Add author count feature to Admin dashboard
    Refactored `AdminStatisticsController` to support fetching author count via an API call using `HttpClient`. Introduced `StatisticsDto` to encapsulate the author count data. Updated `Index.cshtml` to display the author count dynamically, replacing placeholder content. Simplified the UI by removing unused elements. Performed minor cleanup in `StatisticsRepository.cs`.

--- Frontends/CarBook_OnionArch.WebUI/Areas/Admin/Controllers/AdminStatisticsController.cs
+++ Frontends/CarBook_OnionArch.WebUI/Areas/Admin/Controllers/AdminStatisticsController.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook_OnionArch.Dto.StatisticsDtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace CarBook_OnionArch.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class AdminStatisticsController : Controller
+    public class AdminStatisticsController(IHttpClientFactory httpClient) : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index(CancellationToken cancellationToken)
         {
-            return View();
+            StatisticsDto statisticsDto = new StatisticsDto();
+            statisticsDto.authorCount = await GetAuthorCountAsync(cancellationToken);
+            return View(statisticsDto);
+        }
+
+        public async Task<int> GetAuthorCountAsync(CancellationToken cancellationToken)
+        {
+            var client = httpClient.CreateClient();
+            var response = await client.GetAsync("https://localhost:7020/api/Statistics/get-author-count", cancellationToken);
+            if (!response.IsSuccessStatusCode)
+            {
+                return 0;
+            }
+
+            var jsonData = await response.Content.ReadAsStringAsync(cancellationToken);
+            var value = JsonSerializer.Deserialize<StatisticsDto>(jsonData);
+            return value?.authorCount ?? 0;
         }
     }
 }
