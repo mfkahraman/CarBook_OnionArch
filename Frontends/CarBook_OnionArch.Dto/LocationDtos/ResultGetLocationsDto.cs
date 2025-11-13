68e7457bbcda84983d3b67e35feef7820ab4022b
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-11-13 17:32:53 +0300

    Refactor RentalFilterComponent for dynamic locations
    Updated `RentalFilterComponentPartial` to use `IHttpClientFactory` for fetching location data from an external API. Introduced `ResultGetLocationsDto` to represent location data. Modified `Default.cshtml` to replace static input fields with dynamic dropdowns populated from the API response. Added custom styles for form controls. Improved error handling and user experience by making the form more interactive and data-driven.

---
+++ Frontends/CarBook_OnionArch.Dto/LocationDtos/ResultGetLocationsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CarBook_OnionArch.Dto.LocationDtos
+{
+    public class ResultGetLocationsDto
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+    }
+}
--- Frontends/CarBook_OnionArch.WebUI/ViewComponents/RentalFilterComponents/RentalFilterComponentPartial.cs
+++ Frontends/CarBook_OnionArch.WebUI/ViewComponents/RentalFilterComponents/RentalFilterComponentPartial.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook_OnionArch.Dto.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace CarBook_OnionArch.WebUI.ViewComponents.RentalFilterComponents
 {
-    public class RentalFilterComponentPartial : ViewComponent
+    public class RentalFilterComponentPartial(IHttpClientFactory httpClient) 
+        : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var client = httpClient.CreateClient();
+            var response = await client.GetAsync("https://localhost:7020/api/Locations");
+            if (!response.IsSuccessStatusCode)
+            {
+                return Content("Error fetching locations");
+            }
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var locations = JsonSerializer.Deserialize<List<ResultGetLocationsDto>>(jsonData);
+            return View(locations);
         }
     }
 }
